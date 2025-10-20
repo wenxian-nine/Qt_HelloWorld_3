d3ba4ba5979c6b2ffef6be2890f3469a33de0205
wenxian-nine <51015107@gm.nfu.edu.tw>
2025-10-20 15:16:04 +0800

    Project Completed!

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include "designwidget.h"
+
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    DesignWidget w;
+    w.show();
+
+
+    return a.exec();
+}
